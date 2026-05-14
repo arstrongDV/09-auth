730e4b21df5167071d09733a8b1a52b0e0fd9c81
arstrong <arsendacenko89@gmail.com>
2026-05-14 21:29:33 +0200

    NotePreview fix

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -27,7 +27,7 @@ const NotePreview = () => {
                 <div className={css.header}>
                     <h2>{note.title}</h2>
                 </div>
-                <p className={css.tag}>{note.category.name}</p>
+                <p className={css.tag}>{note.tag}</p>
                 <p className={css.content}>{note.content}</p>
                 <p className={css.date}>{note.createdAt}</p>
             </div>
